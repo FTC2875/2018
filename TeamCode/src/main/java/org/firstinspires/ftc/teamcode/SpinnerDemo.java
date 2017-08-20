feb7a02307c94832d0f203dbf9e6567672a11b61
FTC2875 <2875mitibots@gmail.com>
2017-08-20 16:47:17 -0400

    motor flop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinnerDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinnerDemo.java
@@ -80,8 +80,8 @@ public class SpinnerDemo extends LinearOpMode {
 
         // eg: Set the drive motor directions:
         // "Reverse" the motor that runs backwards when connected directly to the battery
-        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        leftMotor.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        rightMotor.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
